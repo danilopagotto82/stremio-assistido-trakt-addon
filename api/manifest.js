f5ec1e2566f78271d5aeac471fcdb50c31d9da1e
danilopagotto82 <59697260+danilopagotto82@users.noreply.github.com>
2025-06-27 22:23:38 -0300

    Create manifest.js

---
+++ api/manifest.js
@@ -0,0 +1,9 @@
+module.exports = () => ({
+  id: 'org.trakt.assistido',
+  version: '1.0.0',
+  name: 'Assistido Trakt',
+  description: 'Mostra se o filme foi assistido no Trakt',
+  resources: ['stream'],
+  types: ['movie'],
+  idPrefixes: ['tt']
+});
