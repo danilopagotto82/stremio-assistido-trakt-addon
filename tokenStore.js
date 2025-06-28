73e5ecc936094741859f714b6c60b796fcb5d1f9
danilopagotto82 <59697260+danilopagotto82@users.noreply.github.com>
2025-06-27 21:51:05 -0300

    Update tokenStore.js

--- tokenStore.js
+++ tokenStore.js
@@ -1,9 +1,12 @@
-const tokens = {};
-
-export async function salvarToken(uid, token) {
-  tokens[uid] = token;
-}
+const tokenFixo = {
+  access_token: 'COLE_AQUI_SEU_TOKEN_TRATK',
+  token_type: 'Bearer',
+  expires_in: 7776000,
+  refresh_token: 'refresh_token_aqui',
+  scope: 'public',
+  created_at: 999999999
+};
 
 export async function buscarToken(uid) {
-  return tokens[uid];
+  return tokenFixo;
 }
